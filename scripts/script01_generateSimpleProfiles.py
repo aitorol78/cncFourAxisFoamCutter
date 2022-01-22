5d3537fa936deda8e395dc2a1610b6eb7eb40178
aitorEtxetik <aitor.ol.zuma@gmail.com>
2022-01-22 20:57:33 +0100

    simple profiles: circle

--- scripts/script01_generateSimpleProfiles.py
+++ scripts/script01_generateSimpleProfiles.py
@@ -2,6 +2,7 @@
 
 import os
 import numpy as np
+import math
 
 # rectangle, 10% 
 x = np.array([  1, 0.01,    0, 0.01, 1])
@@ -16,3 +17,17 @@ for ii in range(len(x)):
 f.close()
 
 
+# circle, 10% 
+radius = 0.5
+xOffset = radius
+angle = np.linspace(0,2*math.pi,100)
+x = radius*np.cos(angle) + xOffset
+y = radius*np.sin(angle)
+xy = np.vstack([x,y])
+
+f = open(os.path.join('data','simpleProfiles','circle.dat'), 'w+')
+f.write("circle, 100%\n")
+for ii in range(len(x)):
+    line = ('    {x:.4f}    {y:.4f}\n').format(x=xy[0,ii], y=xy[1,ii])
+    f.write(line)
+f.close()
