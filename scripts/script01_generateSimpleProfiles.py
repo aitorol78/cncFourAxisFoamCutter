6f9317e4991fc353ef29de0ef6429676008f59f5
aitorEtxetik <aitor.ol.zuma@gmail.com>
2022-01-30 19:02:50 +0100

    trayectory generator: different wire thickness setting at each end

--- scripts/script01_generateSimpleProfiles.py
+++ scripts/script01_generateSimpleProfiles.py
@@ -16,8 +16,20 @@ for ii in range(len(x)):
     f.write(line)
 f.close()
 
+# rectangle, 10% 
+x = np.array([  1, 0.01,    0, 0.01, 1])
+y = np.array([  1,    1,  0.5,    0, 0])
+xy = np.vstack([x,y])
 
-# circle, 10% 
+f = open(os.path.join('data','simpleProfiles','rectangle_100.dat'), 'w+')
+f.write("rectangle, 10%\n")
+for ii in range(len(x)):
+    line = ('    {x:.4f}    {y:.4f}\n').format(x=xy[0,ii], y=xy[1,ii])
+    f.write(line)
+f.close()
+
+
+# circle, 10%
 radius = 0.5
 xOffset = radius
 angle = np.linspace(0,2*math.pi,100)
@@ -26,6 +38,23 @@ y = radius*np.sin(angle)
 xy = np.vstack([x,y])
 
 f = open(os.path.join('data','simpleProfiles','circle.dat'), 'w+')
+f.write("circle, 100%\n")
+for ii in range(len(x)):
+    line = ('    {x:.4f}    {y:.4f}\n').format(x=xy[0,ii], y=xy[1,ii])
+    f.write(line)
+f.close()
+
+
+# semicircle, 10%
+radius = 0.5
+angle = np.linspace(math.pi/2,3*math.pi/2,101)
+x = radius*np.cos(angle)
+y = radius*np.sin(angle)
+x = np.hstack([ 0.5,x, 0.5]) + 0.5
+y = np.hstack([ 0.5,y,-0.5])
+xy = np.vstack([x,y])
+
+f = open(os.path.join('data','simpleProfiles','semicircle.dat'), 'w+')
 f.write("circle, 100%\n")
 for ii in range(len(x)):
     line = ('    {x:.4f}    {y:.4f}\n').format(x=xy[0,ii], y=xy[1,ii])
